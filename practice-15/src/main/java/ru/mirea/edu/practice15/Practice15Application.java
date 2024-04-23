d448f87cdc6a8d02d885038a0c8ccf8c39864bc9
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-04-23 19:09:44 +0300

    feat: add practice 20

--- practice-15/src/main/java/ru/mirea/edu/practice15/Practice15Application.java
+++ practice-15/src/main/java/ru/mirea/edu/practice15/Practice15Application.java
@@ -2,8 +2,10 @@ package ru.mirea.edu.practice15;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Practice15Application {
 
 	public static void main(String[] args) {
