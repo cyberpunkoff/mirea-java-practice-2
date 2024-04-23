0c6bb93a4e29877d1938a56660af4192c546d4fb
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-04-23 19:09:09 +0300

    feat: add practice 19

--- practice-15/src/main/java/ru/mirea/edu/practice15/controller/MarketController.java
+++ practice-15/src/main/java/ru/mirea/edu/practice15/controller/MarketController.java
@@ -1,5 +1,7 @@
 package ru.mirea.edu.practice15.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.mirea.edu.practice15.model.*;
 import ru.mirea.edu.practice15.service.*;
@@ -11,6 +13,7 @@ import java.util.*;
 @RequestMapping("/market")
 public class MarketController {
     private final MarketService marketService;
+    private final Logger logger = LoggerFactory.getLogger(MarketController.class);
 
     public MarketController(MarketService marketService) {
         this.marketService = marketService;
@@ -18,21 +21,25 @@ public class MarketController {
 
     @GetMapping()
     public List<Market> getAllMarkets() {
+        logger.info("Get all markets called!");
         return marketService.getAllMarkets();
     }
 
     @GetMapping("/{id}")
     public Market getMarket(@PathVariable String id) {
+        logger.info("Get market called for id {}", id);
         return marketService.getMarket(id);
     }
 
     @PostMapping()
     public String addMarket(@RequestBody Market market) {
+        logger.info("Adding new market {}", market);
         return marketService.addMarket(market);
     }
 
     @DeleteMapping("/{id}")
     public void deleteMarket(@PathVariable String id) {
+        logger.info("Deleting market {}", id);
         marketService.deleteMarket(id);
     }
 }
--- practice-15/src/main/java/ru/mirea/edu/practice15/controller/ProductController.java
+++ practice-15/src/main/java/ru/mirea/edu/practice15/controller/ProductController.java
@@ -1,17 +1,18 @@
 package ru.mirea.edu.practice15.controller;
 
-import org.springframework.data.repository.query.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import ru.mirea.edu.practice15.model.*;
-import ru.mirea.edu.practice15.service.*;
+import ru.mirea.edu.practice15.model.Product;
+import ru.mirea.edu.practice15.service.ProductService;
 
-
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
     private final ProductService productService;
+    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     public ProductController(ProductService productService) {
         this.productService = productService;
@@ -19,26 +20,31 @@ public class ProductController {
 
     @GetMapping
     public List<Product> getAllProducts() {
+        logger.info("Get all products called!");
         return productService.getAllProducts();
     }
 
     @GetMapping("/filter")
     public List<Product> getAllProductsMoreExpensiveThan(@RequestParam("minPrice") Integer filter) {
+        logger.info("Getting products with price filter more than {}", filter);
         return productService.getProductMoreExpensiveThan(filter);
     }
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable String id) {
+        logger.info("Getting product with id {}", id);
         return productService.getProduct(id);
     }
 
     @PostMapping()
     public String addProduct(@RequestBody Product product) {
+        logger.info("Adding new product {}", product);
         return productService.addProduct(product);
     }
 
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable String id) {
+        logger.info("Deleting product with id {}", id);
         productService.deleteProduct(id);
     }
 }
